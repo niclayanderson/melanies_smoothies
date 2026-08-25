0122fe8cfa6d02893fa7365483ff4c172825bf26
niclayanderson <niclayanderson@gmail.com>
2026-08-25 12:23:35 +1200

    Update streamlit_app.py
    fixed the smoothiefroot API

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
